c57dc9f7823a082e6754ab0892094a66e4475150
Christopher Mickle <cmickle.dev@gmail.com>
2023-12-15 15:07:20 -0600

    Init: Beginning project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CourseGoal from "./components/CourseGoal.tsx";
+export default function App() {
+  return (
+    <main>
+      <CourseGoal
+        title='Learn Typescript with React'
+        description='Learn it in-depth'
+      ></CourseGoal>
+    </main>
+  );
+}
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,16 @@
+type CourseGoalProps = {
+  title: string;
+  description: string;
+};
+
+export default function CourseGoal({ title, description }: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
